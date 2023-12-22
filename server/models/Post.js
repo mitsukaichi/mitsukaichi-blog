d10e427ff481eeb6b73afb72fb8e13ac7b7fdf87
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-21 16:27:04 -0800

    initial commit

---
+++ server/models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+  }
+);
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post;
